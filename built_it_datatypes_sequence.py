6c6c16175fea937437952b439b72eb3a02e2c4b6
osrswati <saraswati@stockbit.com>
2024-12-07 22:08:50 +0700

    rename file

---
+++ built_it_datatypes_sequence.py
@@ -0,0 +1,11 @@
+full_name = 'Saraswati'
+
+for index in range((9)):
+  print(f'Char no-{index} is {full_name[index]}')
+
+# print(len(full_name))
+# print(list(range(9)))
+# print(full_name[1])
+
+# for char in full_name:
+#   print(char)
