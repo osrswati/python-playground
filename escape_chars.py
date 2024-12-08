4ec11b624bfe1c252d0f97a8f887b891ca9a3c9d
osrswati <saraswati@stockbit.com>
2024-12-08 22:44:54 +0700

    escape characters

---
+++ escape_chars.py
@@ -0,0 +1,20 @@
+text = "This \
+is \
+a \
+Text!"
+
+number = 1 + 2 + 3 \
++4
+
+my_full_name = 'Hello \nMy name is Saraswati'
+# my_path = 'C:\Download\node.js'
+my_path = 'C:\Download\\node.js'
+
+
+print(text, number)
+print(my_full_name,my_path)
+print('This is a backslash \\')
+print('Sara\'swati')
+print('Saras\twati')
+print('Saras said \'Hello\" to me')
+print('5$')
