1c2b52452be9cab490cbee22d41588f0e485c139
osrswati <saraswati@stockbit.com>
2024-12-08 22:11:46 +0700

    six types of operators

---
+++ operators.py
@@ -0,0 +1,30 @@
+#arithmetic
+a = 10
+b = 3
+
+print(a/b, a//b, a%b, 10*3, 10**3)
+
+#assignments (assign operators)
+
+# a /= 5
+# b **= 2
+# print(a, b )
+
+#relation
+check_a = a>b
+check_b = b<=a
+print(check_a, check_b)
+
+#unary minus
+print(-a)
+print(-(-a))
+
+#logical
+print(True or False)
+print(a, b, a<b and a>b)
+
+#boolean
+c = False
+is_checked = True
+if is_checked:
+  print('Yes!')
