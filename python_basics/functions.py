f01fbd87c08aed5026d9683db5f63e3645d0c676
osrswati <saraswati@stockbit.com>
2024-12-14 10:20:15 +0700

    practice functions, parameters, and argument

---
+++ python_basics/functions.py
@@ -0,0 +1,56 @@
+#Create a function
+def sum_func(a,b):
+  if type(a) == type(b):
+      return a + b
+  else:
+    print('Datatypes are different, try again!')
+
+sum_calc = sum_func(2,2)
+
+print(sum_calc)
+
+#Different of arguments
+  #position
+  #keyword
+  #default
+  #length arguments
+  
+def data_user(name, age="'Please input your age'"):
+  print(f'Your name is {name} and your age is {age}')
+
+print(
+  #position
+  data_user('Saraswati', 24), 
+  data_user(23, 'Saraswati'),
+  #keyword
+  data_user(age=23, name='Saraswati'),
+  #default
+  data_user('Saras')
+)
+
+#lenth arguments
+def data_student(name, clas, mark):
+  print(f"Your Name: {name}")
+  print(f"Your Class: {clas}")
+  print(f"Your Marks: {mark}")
+
+print(data_student('Saraswati', 'Science Two', 90))
+
+#single
+def data_student(name, clas, *mark):
+  print(f"Your Name: {name}")
+  print(f"Your Class: {clas}")
+  print(f"Your Marks: {mark}")
+
+print(data_student('Saraswati', 'Science Two', 90,100,99,85))
+
+#doulbe *
+def data_student(name, clas, **mark):
+  print(f"Your Name: {name}")
+  print(f"Your Class: {clas}")
+  # print(f"Your Marks: {mark}")
+  print("Marks:")
+  for index, value in mark.items():
+    print(index, value)
+
+print(data_student('Saraswati', 'Science Two', Math=90, English=100, Chemistry=99, Physics=85))
