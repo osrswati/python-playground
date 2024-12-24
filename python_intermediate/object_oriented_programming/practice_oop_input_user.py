43798de7db7ae9c295e88cf68dc258aa0c7870d0
osrswati <saraswati@stockbit.com>
2024-12-24 21:27:38 +0700

    class variabels

--- python_intermediate/object_oriented_programming/practice_oop_input_user.py
+++ python_intermediate/object_oriented_programming/practice_oop_input_user.py
@@ -3,21 +3,24 @@
 # marks= int(input('Please input your marks: '))
 
 class InputData():
+  #class variabels
+  total_student = 0
   
   def __init__(self, name, age=None, **marks): #self always be the first parameter, set default value
     self.name = name
     self.age = age
     self.marks = marks
+    InputData.total_student += 1
   
   def display(self):
     print("Hi", self.name)
     print("Your age", self.age)
     print("Your marks:", self.marks)
 
-# data1 = InputData(name, age, marks)
+data1 = InputData("Saras", 24, science=80)
 
 # data1.display()
 
 data2 = InputData("Jasika", 18, science = 78, sports =94)
 
-data2.display()
+print(InputData.__dict__)
